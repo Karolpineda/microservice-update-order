1faf9d77f7533afa9524c10159691aa58cbacfd7
Jimmy9812 <wladimirmaila1998pm@gmail.com>
2025-02-12 15:16:21 -0500

    feat: :fire: fix

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const orderRoutes = require("./routes/orderRoutes");
 
+app.use(cors({
+    origin: "*", // O especifica "http://localhost:3000"
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 app.use("/api/orders", orderRoutes);
 
