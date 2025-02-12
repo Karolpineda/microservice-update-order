fd9e300918c65012d600cc9ee1e1723e5c0a37bb
Jimmy9812 <wladimirmaila1998pm@gmail.com>
2025-02-12 09:42:10 -0500

    feat:

--- src/models/OrderModel.js
+++ src/models/OrderModel.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/dbOrders");
 
 const Order = sequelize.define(
-  "Order",
+  "Orders",
   {
     id: {
       type: DataTypes.UUID,
